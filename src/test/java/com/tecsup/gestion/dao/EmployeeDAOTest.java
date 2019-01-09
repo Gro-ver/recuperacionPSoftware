456ac93388e160ebd2a70085d8529e34ff1e0412
eduDell5470 <grover.lazaro@tecsup.edu.pe>
2019-01-09 03:20:25 -0500

    test de integracion

--- src/test/java/com/tecsup/gestion/dao/EmployeeDAOTest.java
+++ src/test/java/com/tecsup/gestion/dao/EmployeeDAOTest.java
@@ -42,53 +42,9 @@ public class EmployeeDAOTest {
 		logger.info("Antes de cada metodo");
 	}
 	
-	@Test
-	public void testFindEmployeeById() {
-
-		try {
-			//
-			Employee emp = employeeDAO.findEmployee(100);
-
-			Assert.assertNotNull(emp);
-
-			Assert.assertEquals("jgomez", emp.getLogin());
-			Assert.assertEquals("123456", emp.getPassword());
-			Assert.assertEquals("Jaime", emp.getFirstname());
-			Assert.assertEquals("Gomez", emp.getLastname());
-			Assert.assertEquals(2506, emp.getSalary());
-
-			logger.info(emp.toString());
-
-		} catch (EmptyResultException e) {
-			fail(e.getMessage());
-		} catch (DAOException e) {
-			fail(e.getMessage());
-		}
-
-	}
-	/*
 	
-	@Test
-	public void testFindAllEmployees() {
 
-		try {
-			//
-			List<Employee> emps = employeeDAO.findAllEmployees();
-
-			//Assert.assertEquals(emps.size(), 4);
-
-			logger.info(emps.toString());
-
-		} catch (EmptyResultException e) {
-			fail(e.getMessage());
-		} catch (DAOException e) {
-			fail(e.getMessage());
-		}
-
-		
-	}
-
-/*
+	
 	@Test
 	public void testFindEmployeeByLogin() {
 
@@ -99,7 +55,7 @@ public class EmployeeDAOTest {
 			Assert.assertEquals("123456", emp.getPassword());
 			Assert.assertEquals("Jaime", emp.getFirstname());
 			Assert.assertEquals("Gomez", emp.getLastname());
-			Assert.assertEquals(2505, emp.getSalary());
+			Assert.assertEquals(2506, emp.getSalary());
 
 			logger.info(emp.toString());
 			
@@ -110,114 +66,9 @@ public class EmployeeDAOTest {
 		}
 
 	}
-
-	@Test
-	public void testCreateEmployee() {
-
-		logger.info("--");
-		
-		String LOGIN = "aromero" + (int) (Math.random() * 100);
-		//logger.info(LOGIN);
-		String PWD = "123";
-		String FIRSTNAME = "Alberto";
-		String LASTNAME = "Romero";
-		int SALARY = 3000;
-		int DEPT_ID = 14;
-
-		try {
-			//
-			employeeDAO.create(LOGIN, PWD, FIRSTNAME, LASTNAME, SALARY, DEPT_ID);
-
-			//
-			Employee emp = employeeDAO.findEmployeeByLogin(LOGIN);
-
-			Assert.assertEquals(PWD, emp.getPassword());
-			Assert.assertEquals(FIRSTNAME, emp.getFirstname());
-			Assert.assertEquals(LASTNAME, emp.getLastname());
-			Assert.assertEquals(SALARY, emp.getSalary());
-
-		} catch (DAOException e) {
-			fail(e.getMessage());
-		} catch (EmptyResultException e) {
-			fail(e.getMessage());
-		}
-
-	}
-
 	
 	
-	@Test
-	public void testUpdateEmployee() {
 
-		String LOGIN = "jgomez" + (int)(Math.random() * 100);
-		String PWD = "123";
-		String FIRSTNAME = "Jaime";
-		String LASTNAME = "Gomez";
-		int SALARY = 3000;
-		int DEPT_ID = 14;
-		
-		String UP_PWD = "456";
-		String UP_FIRSTNAME = "Jaime5";
-		String UP_LASTNAME = "Gomez6";
-		int UP_SALARY = 3500;
-		int UP_DEPT_ID = 14;
-		
-		try {
-			//
-			employeeDAO.create(LOGIN, PWD, FIRSTNAME, LASTNAME, SALARY, DEPT_ID);
-			//
-			employeeDAO.update(LOGIN, UP_PWD, UP_FIRSTNAME, UP_LASTNAME, UP_SALARY, UP_DEPT_ID);
-			//
-			Employee emp = employeeDAO.findEmployeeByLogin(LOGIN);
-
-			Assert.assertEquals(UP_PWD, emp.getPassword());
-			Assert.assertEquals(UP_FIRSTNAME, emp.getFirstname());
-			Assert.assertEquals(UP_LASTNAME, emp.getLastname());
-			Assert.assertEquals(UP_SALARY, emp.getSalary());
-
-		} catch (DAOException e) {
-			fail(e.getMessage());
-		} catch (EmptyResultException e) {
-			fail(e.getMessage());
-		}
-
-	}
-	
-	
-	@Test
-	public void testDeleteEmployee() {
-
-		String LOGIN = "jromero";
-		String PWD = "123";
-		String FIRSTNAME = "Juan";
-		String LASTNAME = "Romero";
-		int SALARY = 3000;
-		int DEPT_ID = 14;
-
-		try {
-			//
-			employeeDAO.create(LOGIN, PWD, FIRSTNAME, LASTNAME, SALARY, DEPT_ID);
-
-		} catch (DAOException e) {
-			fail(e.getMessage());
-		}
-
-		try {
-
-			employeeDAO.delete(LOGIN);
-
-			employeeDAO.findEmployeeByLogin(LOGIN);
-
-		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (EmptyResultException e) {
-			// Usuario borrado 
-			return;
-		}
-
-	}
-	*/
 	@After
 	public void after() {
 		logger.info("Despues de cada metodo");
